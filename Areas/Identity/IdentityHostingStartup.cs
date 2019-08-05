988885ac6dc5b265f6404f74439c50c1fc15e8b7
shahingazi <shahinkgc84@gmail.com>
2019-08-05 11:03:50 +0200

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(PriceComparisonWeb.Areas.Identity.IdentityHostingStartup))]
+namespace PriceComparisonWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
